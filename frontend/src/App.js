b9315c1ba6ab26dc49f5a66a32ff862d2b90814c
talhtanveer930 <talha.tanveer@nxb.com.pk>
2022-11-22 19:43:02 +0500

    Login and Register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import Login from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
+
+const router =  createBrowserRouter(createRoutesFromElements(
+  <React.Fragment>
+    <Route path='/Login' element={<Login/>} />
+    <Route path='/Register' element={<Register/>} />
+  </React.Fragment>
+))
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
